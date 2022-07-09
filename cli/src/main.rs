e9889dd5c0b92bac51b8a323e2475326d39a9a67
Kaicho Sun <kaichaosuna@gmail.com>
2022-07-09 14:45:44 +0800

    v0.0.4

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,20 +1,16 @@
 use localtunnel::open_tunnel;
-use tokio::task::JoinHandle;
 
 #[tokio::main]
 async fn main() {
     println!("Run localtunnel CLI!");
-
-    let mut handles: Vec<JoinHandle<()>> = vec![];
 
     let result = open_tunnel(
         Some("http://proxy.ad4m.dev"),
         Some("kaichao"), 
         None, 
         12000,
-        &mut handles
-    ).await;
-    println!("result: {:?}", result);
+    ).await.unwrap();
+    println!("result: {:?}", result.0);
     
-    futures::future::join_all(handles).await;
+    let _ = result.1.await;
 }
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,10 +1,10 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tokio::io::{self, AsyncRead, AsyncWrite};
 use tokio::net::TcpStream;
-use tokio::spawn;
 use tokio::task::JoinHandle;
 use tokio::time::{sleep, Duration};
 
@@ -24,8 +24,7 @@ pub async fn open_tunnel(
     subdomain: Option<&str>,
     local_host: Option<&str>,
     local_port: u16,
-    handles: &mut Vec<JoinHandle<()>>,
-) -> Result<String, Box<dyn std::error::Error>> {
+) -> Result<(String, JoinHandle<()>), Box<dyn std::error::Error>> {
     let server = server.unwrap_or(AD4M_PROXY_SERVER);
     let local_host = local_host.unwrap_or(LOCAL_HOST);
     println!("Start connect to: {}, local port: {}", server, local_port);
@@ -35,46 +34,61 @@ pub async fn open_tunnel(
     let uri = format!("{}/{}", server, assigned_domain);
     println!("Request assign domain: {}", uri);
     let resp = reqwest::get(uri).await?.json::<ProxyResponse>().await?;
-    println!("{:?}", resp);
+    println!("{:#?}", resp);
 
     // Parse and get remote host
     let server_parts = server.split("//").collect::<Vec<&str>>();
     let server_host = server_parts[1];
-    
-    // TODO check the connect is failed and restart the proxy.
 
+    // TODO check the connect is failed and restart the proxy.
     let server_host = server_host.to_string();
     let local_host = local_host.to_string();
-    let handle = spawn(async move {
+    let server_port = resp.port;
+
+    let loop_handle = tokio::spawn(async move {
         let counter = Arc::new(Mutex::new(0));
+
         loop {
             sleep(Duration::from_millis(600)).await;
-    
-            let mut locked_counter = counter.lock().unwrap();
+
+            let mut locked_counter = counter.lock().await;
             if *locked_counter < resp.max_conn_count {
-                println!("spawn new proxy");
+                println!("Create a new proxy connection.");
                 *locked_counter += 1;
-    
+
                 let server_host = server_host.clone();
                 let local_host = local_host.clone();
                 let counter2 = Arc::clone(&counter);
+
                 tokio::spawn(async move {
-                    handle_conn(server_host, resp.port, local_host, local_port, counter2).await
+                    let result = handle_connection(
+                        server_host,
+                        server_port,
+                        local_host,
+                        local_port,
+                        counter2,
+                    )
+                    .await;
+                    println!("Connection result: {:?}", result);
                 });
             }
         }
     });
 
-    handles.push(handle);
-
-    Ok(resp.url)
+    Ok((resp.url, loop_handle))
 }
 
-async fn handle_conn(remote_host: String, remote_port: u16, local_host: String, local_port: u16, counter: Arc<Mutex<u8>>) -> Result<()> {
-    let remote_stream_in = TcpStream::connect(format!("{}:{}", remote_host, remote_port)).await?;
-    let local_stream_in = TcpStream::connect(format!("{}:{}", local_host, local_port)).await?;
+async fn handle_connection(
+    remote_host: String,
+    remote_port: u16,
+    local_host: String,
+    local_port: u16,
+    counter: Arc<Mutex<u8>>,
+) -> Result<()> {
+    let remote_stream = TcpStream::connect(format!("{}:{}", remote_host, remote_port)).await?;
+    let local_stream = TcpStream::connect(format!("{}:{}", local_host, local_port)).await?;
 
-    proxy(remote_stream_in, local_stream_in, counter).await?;
+    proxy(remote_stream, local_stream, counter).await?;
     Ok(())
 }
 
@@ -90,7 +104,9 @@ where
         res = io::copy(&mut s1_read, &mut s2_write) => res,
         res = io::copy(&mut s2_read, &mut s1_write) => res,
     }?;
-    let mut locked_counter = counter.lock().unwrap();
+    let mut locked_counter = counter.lock().await;
+
+    println!("counter: {}", *locked_counter);
     *locked_counter -= 1;
 
     Ok(())
